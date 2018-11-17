5aef9344bcb7e203a47ed5579caa847a56c8e546
Yuta <y.nakagawa1107@gmail.com>
2018-11-17 17:43:44 +0900

    First commit

---
+++ app.py
@@ -0,0 +1,16 @@
+total_price = int(input("合計金額を教えてね(円):"))
+
+number_of_people = int(input("人数を教えてね(人):"))
+print(total_price)
+print(number_of_people)
+
+# print(total_price / number)
+
+# print(type(total_price))
+# print(type(number_of_people))
+
+# print(total_price / number_of_people)
+
+# print(f"1人あたり: {amount // number_of_people}円, 端数: {aprint(f"1人あたり: {amount // number_of_people}円, 端数
+print(f"1人あたり: {total_price // number_of_people}円, 端数: {total_price % number_of_people}円")
+
